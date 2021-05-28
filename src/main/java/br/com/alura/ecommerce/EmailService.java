257148a535a2576a0828cafbcaf0aeb8fd2e3faf
Luiz Moraes <lffm1994@gmail.com>
2021-05-28 16:14:47 -0300

    Modulo 2 - Paralelizando tarefas em um serviço

--- src/main/java/br/com/alura/ecommerce/EmailService.java
+++ src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -6,24 +6,22 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Properties;
-import java.util.UUID;
 
-public class FraudDetectorService {
+public class EmailService {
 
     public static void main(String[] args) {
         var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
         //colocar a chamada em um laco de repetição
-        while(true) {
+        while (true) {
             var records = consumer.poll(Duration.ofMillis(100));
             if (!records.isEmpty()) {
                 System.out.println("Encontrei " + records.count() + " registros");
 
                 for (var record : records) {
                     System.out.println("----------------------------------------");
-                    System.out.println("Processing new order, checking for fraud");
+                    System.out.println("Send email");
                     System.out.println(record.key());
                     System.out.println(record.value());
                     System.out.println(record.partition());
@@ -34,8 +32,8 @@ public class FraudDetectorService {
                         // ignoring
                         e.printStackTrace();
                     }
-                    System.out.println("Order processed");
-               }
+                    System.out.println("Email send");
+                }
             }
         }
     }
@@ -46,9 +44,7 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "_" + UUID.randomUUID().toString());
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG , "1");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
         return properties;
     }
 }
